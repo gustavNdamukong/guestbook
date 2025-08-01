aac7189f95bd22a050267c3145f147d557648673
Gustav Ndamukong <gustavfn@yahoo.co.uk>
2025-08-01 13:28:34 -0400

    Fixed issue of duplicate relation 'admin_id_seq' when running migrations

--- migrations/Version20250801021846.php
+++ migrations/Version20250801021846.php
@@ -20,7 +20,6 @@ final class Version20250801021846 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SEQUENCE admin_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE admin (id INT NOT NULL, username VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_IDENTIFIER_USERNAME ON admin (username)');
     }
